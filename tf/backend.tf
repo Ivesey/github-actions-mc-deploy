a21c97a2e959320b44e62176e8c1dd44c692af86
Daniel Ives <dives@qa.com>
2025-08-28 10:20:38 +0100

    added workflows

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
     bucket = "tf-backend-bucket-040125"
-    key    = "multicloud-iac-cicd-d03-tf-state-file"
+    key    = "multicloud-github-actions-tf-state-file"
   }
 }
--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,7 +1,7 @@
-output "aws_dns" {
-  value = "http://${module.aws.public_dns}"
-}
+# output "aws_dns" {
+#   value = "http://${module.aws.public_dns}"
+# }
 
-output "azure_dns" {
-  value = "http://${module.azure.public_dns}"
-}
+# output "azure_dns" {
+#   value = "http://${module.azure.public_dns}"
+# }
